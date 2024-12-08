aad7232bd9b369c98c99c9fabe668436d5ecd8bc
Fizan Muhammed Faisal <fizanmuhammedfaisal.k@gmail.com>
2024-12-08 21:06:22 +0530

    added the inital message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 function Home() {
-  return <div className='text-center'>Fizan </div>
+  return (
+    <div className='min-h-screen flex justify-center bg-black'>
+      <div className='flex justify-center items-center text-green-500 font-bold'>
+        <p>
+          {' '}
+          Hello there , I'm Fizan Muhammed Faisal and This Portfolio is Under
+          construction
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
