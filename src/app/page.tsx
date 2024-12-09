e7c231e9580e5825595b9d898a0cb9800088459a
Fizan Muhammed Faisal <fizanmuhammedfaisal.k@gmail.com>
2024-12-09 09:11:19 +0530

    added the inital message-updated the error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+'use client'
+import { motion } from 'motion/react'
 import React from 'react'
 
 function Home() {
   return (
     <div className='min-h-screen flex justify-center bg-black'>
-      <div className='flex justify-center items-center text-green-500 font-bold'>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+        className='flex justify-center items-center text-green-500 font-bold'
+      >
         <p>
           {' '}
           Hello there , I'm Fizan Muhammed Faisal and This Portfolio is Under
           construction
         </p>
-      </div>
+      </motion.div>
     </div>
   )
 }
